b66deb588d6c950d87f9943fde43fa359bf25392
charlesnapster65@gmail.com <charlesnapster65@gmail.com>
2017-07-13 18:01:55 -0400

    features

--- imports/ui/Team-stats.jsx
+++ imports/ui/Team-stats.jsx
@@ -9,6 +9,44 @@ export default class TeamStats extends Component {
     const players = this.props.players;
     const numPlayers = players.length;
 
+    // const to = get a total for ballManipulation
+    // const possible =  3 * numPlayers
+
+    const ballManipulation = Math.round((players.reduce((ballManipulation, player) => {
+      return ballManipulation + player.ballManipulation;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const kickingAbilities = Math.round((players.reduce((kickingAbilities, player) => {
+      return kickingAbilities + player.kickingAbilities;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const passingAbilities = Math.round((players.reduce((passingAbilities, player) => {
+      return passingAbilities + player.passingAbilities;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const duelTackling = Math.round((players.reduce((duelTackling, player) => {
+      return duelTackling + player.duelTackling;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const fieldCoverage = Math.round((players.reduce((fieldCoverage, player) => {
+      return fieldCoverage + player.fieldCoverage;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const blockingAbilities = Math.round((players.reduce((blockingAbilities, player) => {
+      return blockingAbilities + player.blockingAbilities;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const gameStrategy = Math.round((players.reduce((gameStrategy, player) => {
+      return gameStrategy + player.gameStrategy;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const playmakingRisks = Math.round((players.reduce((playmakingRisks, player) => {
+      return playmakingRisks + player.playmakingRisks;
+    }, 0) / ( 3 * numPlayers)) * 100);
+
+    const defense = Math.round((duelTackling + fieldCoverage + blockingAbilities + gameStrategy + playmakingRisks)/5);
+    const offense = Math.round((kickingAbilities + ballManipulation + passingAbilities + fieldCoverage + gameStrategy + playmakingRisks)/6);
+    const total =  Math.round((kickingAbilities + ballManipulation + passingAbilities + fieldCoverage + gameStrategy + playmakingRisks + duelTackling + blockingAbilities)/8);
 
     const data = {
       labels: ['Ball Manipulation', 'Kicking Abilities', 'Passing Abilities', 'Duel/Tackling', 'Field Coverage', 'Blocking Abilities', 'Game Strategy', 'Playmaking Risks' ],
@@ -21,7 +59,7 @@ export default class TeamStats extends Component {
           pointBorderColor: '#fff',
           pointHoverBackgroundColor: '#fff',
           pointHoverBorderColor: 'rgba(12, 71, 161, 1)',
-          data: [65, 59, 90, 81, 56, 55, 40]
+          data: [ballManipulation, kickingAbilities, passingAbilities, duelTackling, fieldCoverage, blockingAbilities, gameStrategy, playmakingRisks]
         }
       ]
     };
@@ -41,11 +79,11 @@ export default class TeamStats extends Component {
           <div className="col s12 m5">
             <h4>Scores in % of max possible</h4>
             <Divider />
-            <h4>Team's offense: 45%</h4>
-            <h4>Team's defense: 45%</h4>
-            <h4>Team's total: 45%</h4>
+            <h4>Team's offense: {offense}%</h4>
+            <h4>Team's defense: {defense}%</h4>
+            <h4>Team's total: {total}%</h4>
             <Divider />
-            <h4>Number of players: 9</h4>
+            <h4>Number of players: {numPlayers}</h4>
           </div>
         </div>
       </div>
